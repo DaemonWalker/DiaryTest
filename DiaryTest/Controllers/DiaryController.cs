1d26f614da5101bced061f31c3949e386a9cd418
St_Kalecgos <215136855@qq.com>
2020-12-02 00:01:33 +0800

    添加项目文件。

---
+++ DiaryTest/Controllers/DiaryController.cs
@@ -0,0 +1,29 @@
+﻿using DiaryTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiaryTest.Controllers
+{
+    public class DiaryController : Controller
+    {
+        private readonly ILogger<DiaryController> _logger;
+
+        public DiaryController(ILogger<DiaryController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [Route("[controller]/{id}")]
+        public IActionResult Index(string id)
+        {
+            ViewBag.Title = id;
+            return View();
+        }
+    }
+}
